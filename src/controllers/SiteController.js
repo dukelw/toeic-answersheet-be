44fd0af5eeaba7e26d22204b50a07e630b38e4d6
dukelw <thevi16102004@gmail.com>
2024-08-29 22:02:18 +0700

    test

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,7 +1,7 @@
 const AnswerController = require("./AnswerController");
 class SiteController {
   async welcome(req, res, next) {
-    const result = await AnswerController.getAll();
+    const result = await AnswerController.getAll("TOEIC");
     res.status(200).send(result);
   }
 }
